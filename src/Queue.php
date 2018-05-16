2a035f2925ba64699fb9ddadb65c6a06ecb303e2
gaojian1 <gaojian1@chelun.com>
2018-05-16 10:54:42 +0800

    队列文件处理

--- src/Queue.php
+++ src/Queue.php
@@ -6,7 +6,7 @@ namespace Jeanku\Rabbitmq;
  * @desc more description
  * @date 2018-04-02
  */
-abstract class Consume
+abstract class Queue
 {
 
     protected $exchange = '';
@@ -53,6 +53,30 @@ abstract class Consume
         }
         $conn->disconnect();
     }
+
+
+    /**
+     * message push
+     * @date 2018-04-11
+     * @param string|Closure $message require 信息
+     * @param string $exchangeName sometime 交换机key
+     * @param string $route sometime 路由key
+     * @return array
+     */
+    public static function push($message, $exchangeName = 'master', $route = '')
+    {
+        $conn = Connection::getInstance();
+        $channel = new \AMQPChannel($conn);
+        $exchange = new \AMQPExchange($channel);
+        $exchange->setName($exchangeName);
+        if (!empty($route)) {
+            $exchange->publish($message, $route);
+        } else {
+            $exchange->publish($message);
+        }
+        $conn->disconnect();
+    }
+
 
     /**
      * handle message
